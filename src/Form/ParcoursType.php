a2dcf943d7e716f0d2f0f1f011bf9dd118df0726
LudovicCocquyt <cocquyt.ludovic@laposte.net>
2019-07-16 11:18:32 +0200

    creation crud

---
+++ src/Form/ParcoursType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Parcours;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParcoursType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('poste')
+            ->add('lieu')
+            ->add('dateStart')
+            ->add('dateEnd')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Parcours::class,
+        ]);
+    }
+}
